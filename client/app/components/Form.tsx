c80f48ea265cfe74bfd22a741eb5cb7acdc39743
Athiwat07122543 <athiwat07122543@gmail.com>
2025-12-03 22:37:26 +0700

    Update UI/UX and language

--- client/app/components/Form.tsx
+++ client/app/components/Form.tsx
@@ -2,6 +2,13 @@
 import { useEffect, useState } from "react"
 import socket from "../socket";
 import { toast } from 'react-toastify';
+import { Monda } from 'next/font/google';
+
+const monda = Monda({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+
 type Data = {
     id: string | undefined,
     firstName: string,
@@ -44,7 +51,7 @@ export default function Form() {
         const update = { ...data, status: "submitted" }
         socket.emit("form", update)
         setData(initialData)
-        toast.success("ส่งข้อมูลผู้ป่วยสำเร็จ")
+        toast.success("Patient information submitted successfully.")
         socket.disconnect();
         socket.connect();
     }
@@ -74,80 +81,80 @@ export default function Form() {
         return () => clearTimeout(timer);
     }, [data]);
 
-    
+
     return (
-        <div className="bg-white p-2 md:mt-0 md:p-10 rounded-xl shadow-2xl min-w-[300px] md:w-[550px]">
-            <label className="font-bold text-xl md:text-4xl text-center block w-full">แบบฟอร์มกรอกข้อมูลผู้ป่วย</label>
+        <div className={`bg-white p-2 md:mt-0 md:p-10 rounded-xl shadow-2xl min-w-[300px] md:w-[550px] ${monda.className}`}>
+            <label className="font-bold text-xl md:text-4xl text-center block w-full">Patient Information Form</label>
             <form onSubmit={handleSubmit} className="flex flex-col md:gap-4 md:p-4">
                 <div className="md:flex md:gap-4">
                     <div className="flex flex-col">
-                        <label><label className="text-red-500">*</label>ชื่อจริง</label>
-                        <input name="firstName" type="text" onChange={handleForm} value={data.firstName} className="border py-1 px-2 rounded-md" required onInvalid={(e) => e.currentTarget.setCustomValidity('กรุณาใส่ข้อมูลชื่อจริง')} onInput={(e) => e.currentTarget.setCustomValidity('')} />
+                        <label><label className="text-red-500">*</label>First name</label>
+                        <input name="firstName" type="text" onChange={handleForm} value={data.firstName} className="border py-1 px-2 rounded-md" required />
                     </div>
                     <div className="flex flex-col">
-                        <label><label className="text-red-500">*</label>นามสกุล</label>
-                        <input name="lastName" type="text" onChange={handleForm} value={data.lastName} className="border py-1 px-2 rounded-md" required onInvalid={(e) => e.currentTarget.setCustomValidity('กรุณาใส่ข้อมูลนามสกุล')} onInput={(e) => e.currentTarget.setCustomValidity('')} />
+                        <label><label className="text-red-500">*</label>Last name</label>
+                        <input name="lastName" type="text" onChange={handleForm} value={data.lastName} className="border py-1 px-2 rounded-md" required />
                     </div>
                 </div>
                 <div className="md:flex gap-4">
                     <div className="flex flex-col">
-                        <label>ชื่อเล่น</label>
+                        <label>Middle name (Option)</label>
                         <input name="middleName" type="text" onChange={handleForm} value={data.middleName} className="border py-1 px-2 rounded-md" />
                     </div>
                     <div className="flex flex-col md:w-full">
-                        <label><label className="text-red-500">*</label>วันเดือนปีเกิด</label>
-                        <input name="birthday" type="Date" onChange={handleForm} value={data.birthday} className="border py-1 px-2 rounded-md" required onInvalid={(e) => e.currentTarget.setCustomValidity('กรุณาใส่ข้อมูลวันเดือนปีเกิด')} onInput={(e) => e.currentTarget.setCustomValidity('')} />
+                        <label><label className="text-red-500">*</label>Date of Birth</label>
+                        <input name="birthday" type="Date" onChange={handleForm} value={data.birthday} className="border py-1 px-2 rounded-md" required />
                     </div>
                 </div>
                 <div className="md:flex gap-4">
                     <div className="flex flex-col md:w-full">
-                        <label><label className="text-red-500">*</label>เพศ</label>
+                        <label><label className="text-red-500">*</label>Gender</label>
                         <select name="gender" onChange={handleForm} className="border py-[6.4] px-2 rounded-md" value={data.gender}>
-                            <option value="man">ผู้ชาย</option>
-                            <option value="girl">ผู้หญิง</option>
+                            <option value="man">Man</option>
+                            <option value="girl">Female</option>
                         </select>
                     </div>
                     <div className="flex flex-col">
-                        <label><label className="text-red-500">*</label>เบอร์โทรศัพท์</label>
-                        <input name="phoneNumber" type="text" inputMode="numeric" pattern="0[0-9]{9}" onChange={handleForm} value={data.phoneNumber} className="border py-1 px-2 rounded-md" required onInvalid={(e) => e.currentTarget.setCustomValidity('กรุณาใส่ข้อมูลเบอร์โทรศัพท์ที่ถูกต้อง')} onInput={(e) => e.currentTarget.setCustomValidity('')} />
+                        <label><label className="text-red-500">*</label>Phone number</label>
+                        <input name="phoneNumber" type="text" inputMode="numeric" pattern="0[0-9]{9}" onChange={handleForm} value={data.phoneNumber} className="border py-1 px-2 rounded-md" required />
                     </div>
                 </div>
                 <div className="flex flex-col">
-                    <label><label className="text-red-500">*</label>อีเมล</label>
-                    <input name="email" type="email" onChange={handleForm} value={data.email} className="border py-1 px-2 rounded-md" required onInvalid={(e) => e.currentTarget.setCustomValidity('กรุณาใส่ข้อมูลอีเมลให้ถูกต้อง')} onInput={(e) => e.currentTarget.setCustomValidity('')} />
+                    <label><label className="text-red-500">*</label>Email</label>
+                    <input name="email" type="email" onChange={handleForm} value={data.email} className="border py-1 px-2 rounded-md" required />
                 </div>
                 <div className="flex flex-col">
-                    <label className="w-10"><label className="text-red-500">*</label>ที่อยู่</label>
-                    <textarea name="address" onChange={handleForm} value={data.address} className="border py-1 px-2 rounded-md md:w-full" rows={2} required onInvalid={(e) => e.currentTarget.setCustomValidity('กรุณาใส่ข้อมูลที่อยู่')} onInput={(e) => e.currentTarget.setCustomValidity('')} />
+                    <label className="w-10"><label className="text-red-500">*</label>Address</label>
+                    <textarea name="address" onChange={handleForm} value={data.address} className="border py-1 px-2 rounded-md md:w-full" rows={2} required />
                 </div>
 
 
                 <div className="md:flex gap-4">
                     <div className="flex flex-col">
-                        <label><label className="text-red-500">*</label>สัญชาติ</label>
-                        <input name="nationality" type="text" onChange={handleForm} value={data.nationality} className="border py-1 px-2 rounded-md" required onInvalid={(e) => e.currentTarget.setCustomValidity('กรุณาใส่ข้อมูลสัญชาติ')} onInput={(e) => e.currentTarget.setCustomValidity('')} />
+                        <label><label className="text-red-500">*</label>Nationlity</label>
+                        <input name="nationality" type="text" onChange={handleForm} value={data.nationality} className="border py-1 px-2 rounded-md" required />
                     </div>
                     <div className="flex flex-col">
-                        <label><label className="text-red-500">*</label>ภาษาที่ต้องการใช้</label>
-                        <input name="language" type="text" onChange={handleForm} value={data.language} className="border py-1 px-2 rounded-md" required onInvalid={(e) => e.currentTarget.setCustomValidity('กรุณาใส่ภาษาที่ต้องการ')} onInput={(e) => e.currentTarget.setCustomValidity('')} />
+                        <label><label className="text-red-500">*</label>Preferred Language</label>
+                        <input name="language" type="text" onChange={handleForm} value={data.language} className="border py-1 px-2 rounded-md" required />
                     </div>
                 </div>
 
-                <div>
-                    <label className="text-[20px] font-bold">ผู้ติดต่อกรณีฉุกเฉิน</label>
-                    <div className="md:flex gap-4 mt-2">
+                <div className=" mt-2 md:mt-0">
+                    <label className="text-[20px] font-bold">Emergency Contact</label>
+                    <div className="md:flex gap-4">
                         <div className="flex flex-col">
-                            <label>ชื่อ</label>
+                            <label>Name</label>
                             <input name="emergencyName" type="text" onChange={handleForm} value={data.emergencyName} className="border py-1 px-2 rounded-md" />
                         </div>
                         <div className="flex flex-col">
-                            <label>ความสัมพันธ์</label>
+                            <label>Relationshop</label>
                             <input name="emergencyRelation" type="text" onChange={handleForm} value={data.emergencyRelation} className="border py-1 px-2 rounded-md" />
                         </div>
                     </div>
                 </div>
 
-                <button type="submit" className="bg-green-500 w-fit p-2 mt-2 rounded-md text-white hover:cursor-pointer hover:bg-green-600">ส่งข้อมูลผู้ป่วย</button>
+                <button type="submit" className="bg-green-500 w-fit p-2 mt-2 rounded-md text-white hover:cursor-pointer hover:bg-green-600">Send</button>
 
 
             </form>
--- client/app/components/list.tsx
+++ client/app/components/list.tsx
@@ -1,6 +1,12 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import socket from "../socket";
+import { Monda } from 'next/font/google';
+
+const monda = Monda({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
 
 type Data = {
     id: string,
@@ -46,103 +52,131 @@ export default function List() {
     }, [])
 
     return (
-        <div className="rounded-xl shadow-2xl md:p-10 bg-white p-2">
+        <div className={`rounded-xl shadow-2xl md:p-10 bg-white p-2 ${monda.className}`}>
             <div className='py-4'>
-                <label className='font-bold text-2xl md:text-6xl text-left'>ข้อมูลผู้ป่วย</label>
+                <label className='font-bold text-2xl md:text-6xl text-left'>Patient Information</label>
             </div>
-            <div className='flex flex-col md:justify-between'>
-                <div>
-                    <label>จำนวนข้อมูลผู้ป่วย:</label>
-                    <label>{data.length}</label>
-                </div>
-                <div>
-                    <label>จำนวนข้อมูลที่ส่งฟอร์มแล้ว:</label>
-                    <label>{submittedCount}</label>
-                </div>
-                <div>
-                    <label>จำนวนข้อมูลที่กำลังกรอกข้อมูลอยู่:</label>
-                    <label>{activelyCount}</label>
-                </div>
-                <div>
-                    <label>จำนวนข้อมูลที่ไม่ได้ใช้งานฟอร์ม:</label>
-                    <label>{inactiveCount}</label>
+
+            {/* mobile */}
+            <div className='block md:hidden'>
+                <div className='flex flex-col md:justify-between'>
+                    <div>
+                        <label>Total Patients:</label>
+                        <label>{data.length}</label>
+                    </div>
+                    <div>
+                        <label>Submitted Forms:</label>
+                        <label>{submittedCount}</label>
+                    </div>
+                    <div>
+                        <label>Forms In Progress:</label>
+                        <label>{activelyCount}</label>
+                    </div>
+                    <div>
+                        <label>Inactive Forms:</label>
+                        <label>{inactiveCount}</label>
+                    </div>
                 </div>
             </div>
+
+            {/* Desktop */}
+            <div className='hidden md:block'>
+                <div className='flex justify-center gap-4'>
+                    <div className='border p-2 border-orange-500 bg-orange-500 text-white text-xl rounded-md'>
+                        <label>Total Patients:</label>
+                        <label>{data.length}</label>
+                    </div>
+                    <div className='border p-2 border-green-600 bg-green-600 text-white text-xl rounded-md'>
+                        <label>Submitted Forms:</label>
+                        <label>{submittedCount}</label>
+                    </div>
+                    <div className='border p-2 border-blue-500 bg-blue-500 text-white text-xl rounded-md'>
+                        <label>Forms In Progress:</label>
+                        <label>{activelyCount}</label>
+                    </div>
+                    <div className='border p-2 border-red-500 bg-red-500 text-white text-xl rounded-md'>
+                        <label>Inactive Forms:</label>
+                        <label>{inactiveCount}</label>
+                    </div>
+                </div>
+            </div>
+
             <ul className='md:flex md:flex-col md:gap-2 space-y-2 md:space-y-0 mt-2'>
                 {data.length <= 0
                     ? <div className='flex justify-center py-4'>
-                        <label className='text-xl md:text-2xl'>ไม่มีข้อมูลผู้ป่วย</label>
+                        <label className='text-xl md:text-2xl'>No patient data available.</label>
                     </div>
                     : (data.map((item, index) =>
                         <li className='p-2 md:p-4 border border-orange-200 rounded-md' key={index}>
                             <div>
                                 <label className={`font-bold md:text-4xl ${item.status === "submitted" && "text-green-500"} ${item.status === "actively" && "text-blue-500"} ${item.status === "inactive" && "text-red-500"}`}>
-                                    {item.status === "submitted" && "ส่งฟอร์มแล้ว"}
-                                    {item.status === "actively" && "กำลังกรอกข้อมูลอยู่"}
-                                    {item.status === "inactive" && "ไม่ได้ใช้งานฟอร์ม"}
+                                    {item.status === "submitted" && "Form submitted"}
+                                    {item.status === "actively" && "Form in progress"}
+                                    {item.status === "inactive" && "Form inactive"}
+
                                 </label>
                                 <div className="flex gap-1 md:gap-4 flex-wrap">
 
                                     <div className="flex flex-col basis-[34%] md:basis-[6%]">
-                                        <label>ชื่อจริง</label>
+                                        <label>First name</label>
                                         <label className="border py-1 px-2 rounded-md min-h-8.5">{item.firstName}</label>
                                     </div>
 
                                     <div className="flex flex-col basis-[34%] md:basis-[6%]">
-                                        <label>นามสกุล</label>
+                                        <label>Last name</label>
                                         <label className="border py-1 px-2 rounded-md min-h-8.5">{item.lastName}</label>
                                     </div>
 
                                     <div className="flex flex-col basis-[29%] md:basis-[6%]">
-                                        <label>ชื่อเล่น</label>
+                                        <label>Middle name</label>
                                         <label className="border py-1 px-2 rounded-md min-h-8.5">{item.middleName}</label>
                                     </div>
 
                                     <div className="flex flex-col basis-[38%] md:basis-[6%]">
-                                        <label className='min-w-26'>วันเดือนปีเกิด</label>
+                                        <label className='min-w-26'>Date of Birth</label>
                                         <label className="border py-1 px-2 rounded-md min-h-8.5">{item.birthday}</label>
                                     </div>
 
 
                                     <div className="flex flex-col basis-[20%] md:basis-[4%]">
-                                        <label>เพศ</label>
-                                        <label className="border py-1 px-2 rounded-md min-h-8.5">{item.gender === "man" ? "ผู้ชาย" : "ผู้หญิง"}</label>
+                                        <label>Gender</label>
+                                        <label className="border py-1 px-2 rounded-md min-h-8.5">{item.gender === "man" ? "Man" : "Female"}</label>
                                     </div>
 
                                     <div className="flex flex-col basis-[38%] md:basis-[10%]">
-                                        <label>เบอร์โทรศัพท์</label>
+                                        <label>Phone number</label>
                                         <label className="border py-1 px-2 rounded-md min-h-8.5">{item.phoneNumber}</label>
                                     </div>
 
                                     <div className="flex flex-col basis-full md:basis-[14%]">
-                                        <label>อีเมล</label>
+                                        <label>Email</label>
                                         <label className="border py-1 px-2 rounded-md min-h-8.5">{item.email}</label>
                                     </div>
 
                                     <div className="flex flex-col basis-[49%] md:basis-[6%]">
-                                        <label>สัญชาติ</label>
+                                        <label>Nationality</label>
                                         <label className="border py-1 px-2 rounded-md min-h-8.5">{item.nationality}</label>
                                     </div>
 
-                                    <div className="flex flex-col basis-[49%] md:basis-[6%]">
-                                        <label className='min-w-30'>ภาษาที่ต้องการใช้</label>
+                                    <div className="flex flex-col basis-[49%] md:basis-[8%]">
+                                        <label className='min-w-30'>Preferred Language</label>
                                         <label className="border py-1 px-2 rounded-md min-h-8.5">{item.language}</label>
                                     </div>
 
                                     <div className="flex flex-col basis-full md:basis-[26%]">
-                                        <label className="w-10">ที่อยู่</label>
+                                        <label className="w-10">Address</label>
                                         <label className="border py-1 px-2 rounded-md min-h-8.5">{item.address}</label>
                                     </div>
                                 </div>
                                 <div className='md:mt-2'>
-                                    <label className="text-[20px] font-bold">ผู้ติดต่อกรณีฉุกเฉิน</label>
+                                    <label className="text-[20px] font-bold">Emergency Contact</label>
                                     <div className="flex gap-1 md:gap-4 flex-wrap">
                                         <div className="flex flex-col basis-[49%] md:basis-[6%]">
-                                            <label>ชื่อ</label>
+                                            <label>Name</label>
                                             <label className="border py-1 px-2 rounded-md min-h-8.5">{item.emergencyName}</label>
                                         </div>
                                         <div className="flex flex-col basis-[49%] md:basis-[6%]">
-                                            <label>ความสัมพันธ์</label>
+                                            <label>Relationshop</label>
                                             <label className="border py-1 px-2 rounded-md min-h-8.5">{item.emergencyRelation}</label>
                                         </div>
                                     </div>
